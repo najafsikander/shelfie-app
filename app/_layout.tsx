7de2c12993e292803abb59d46ecba582f2267a61
najafsikander <najafsikander@yahoo.com>
2025-04-19 14:08:19 +0400

    Added stack implementation properly

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
-import { Stack } from 'expo-router'
+import { Stack } from "expo-router";
+import { View, Text } from "react-native";
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{
+        headerStyle: {
+            backgroundColor: '#ddd'
+        },
+        headerTintColor: '#333',
+    }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          title: "About",
+        }}
+      />
+      <Stack.Screen
+        name="contact"
+        options={{
+          title: "Contact",
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 import Logo from "../assets/img/logo_light.png";
 const Home = () => {
   return (
     <View style={styles.container}>
-        <Image source={Logo} style={styles.img}/>
-      <Text style={[styles.title, {color: 'purple'}]}>The Number 1</Text>
+      <Image source={Logo} style={styles.img} />
+      <Text style={[styles.title, { color: "purple" }]}>The Number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <Link href="/about" style={styles.link}>About Page</Link>
+      <Link href="/contact" style={styles.link}>Contact Page</Link>
     </View>
   );
 };
@@ -14,16 +17,21 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-  img:{
-    marginVertical: 20
-  }
-});
+    container: {
+      backgroundColor: '#e0dfe8',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    img: {
+      marginVertical: 20
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    link: {
+      marginVertical: 10,
+      borderBottomWidth: 1
+    }
+  })
