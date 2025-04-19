a5da81a2237fa97dc93d86a0f32ce5fb1f4d338d
najafsikander <najafsikander@yahoo.com>
2025-04-19 13:11:59 +0400

    first commit

---
+++ app/declaration.d.ts
@@ -0,0 +1,10 @@
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
+
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
