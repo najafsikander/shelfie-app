8931b08e1bec64f3f7c6e641a1af01743cd728a0
najafsikander <najafsikander@yahoo.com>
2025-04-24 17:28:18 +0400

    Added safeview

--- app/components/ThemedView.tsx
+++ app/components/ThemedView.tsx
@@ -1,11 +1,17 @@
 import { View, useColorScheme, StyleProp, ViewStyle } from "react-native";
 import { Colors } from "../../constants/Colors";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const ThemedView: React.FC<React.PropsWithChildren<{ style?: StyleProp<ViewStyle> }>> = ({ style, children, ...props }) => {
+const ThemedView: React.FC<React.PropsWithChildren<{ style?: StyleProp<ViewStyle>, safe?:boolean }>> = ({ style, safe=false, children, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme ?? "light"];
-  return <View style={[{backgroundColor: theme.background}, style]} {...props}>{children}</View>;
+  if(!safe)return( <View style={[{backgroundColor: theme.background}, style]} {...props}>{children}</View>)
+
+  const insets = useSafeAreaInsets();
+  return(
+    <View style={[{backgroundColor: theme.background, paddingTop:insets.top, paddingBottom:insets.bottom}, style]} {...props}>{children}</View>
+  )
 };
 
 export default ThemedView;
