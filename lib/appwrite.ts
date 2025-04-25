4388d18893250a0a9ccea829377cb68c9a6bc061
najafsikander <najafsikander@yahoo.com>
2025-04-25 12:58:27 +0400

    Added appwrite config

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+import { Client, Account, Avatars } from 'react-native-appwrite';
+
+export const client = new Client()
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject('6803562a0036caf65aee')
+    .setPlatform('com.najaf.shelfie');
+
+export const account = new Account(client);
+export const avatars = new Avatars(client);
