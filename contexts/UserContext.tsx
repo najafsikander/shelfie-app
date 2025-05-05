8c906c9374865bfd6200dc86695dd6a12b2ea884
najafsikander <najafsikander@yahoo.com>
2025-05-05 17:17:51 +0400

    Added intial value checker

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react';
+import {createContext, useEffect, useState} from 'react';
 import React from 'react';
 import {account} from '../lib/appwrite';
 import { ID } from 'react-native-appwrite';
@@ -13,12 +13,14 @@ interface UserContextType {
     login: (userData: any) => void;
     register: (userData: any) => void;
     logout: () => void;
+    authChecked: boolean;
 }
 
 export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
     const [user, setUser] = useState<UserData | any | null>(null);
+    const [authChecked, setAuthChecked] = useState<boolean>(false);
     
     const login = async (userData:UserData) => {
         try {
@@ -46,9 +48,29 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({children}
         await account.deleteSession('current');
         setUser(null);
     };
+
+    const getInitialUserValue = async () => {
+        try {
+            console.log("Getting initial user value");
+            const userResponse = await account.get();
+            console.log("User response: ", userResponse);
+            setUser(userResponse);
+        } catch (error:any) {
+            console.error("Error getting initial user value: ", error);
+            setUser(null);
+            throw Error(error.message);
+        } finally {
+            setAuthChecked(true);
+        }
+    }
+
+    useEffect(() => {
+        console.log("useEffect for getting initial user value");
+        getInitialUserValue();
+    }, []);
     
     return (
-        <UserContext.Provider value={{ user, login, register, logout }}>
+        <UserContext.Provider value={{ user, login, register, logout, authChecked }}>
         {children}
         </UserContext.Provider>
     );
